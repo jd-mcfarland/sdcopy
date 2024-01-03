c9d59f423603bfe1a1db5f8c5baa82db605acdbf
jd-mcfarland <155583788+jd-mcfarland@users.noreply.github.com>
2024-01-03 16:02:37 -0500

    Update asyncronous.sh
    anonymized

--- asyncronous.sh
+++ asyncronous.sh
@@ -2,7 +2,7 @@
 
 # Define the mount point of the SD card and log file location
 MOUNT_POINT="/media/nas"
-LOG_FILE="/home/nas/Documents/copysdlogs/copysdlogs.log"
+LOG_FILE="[PATH TO LOGS]/copysdlogs.log"
 
 # Log the start of the process
 echo "Executing user: $(whoami)" >> "$LOG_FILE"
@@ -11,7 +11,7 @@ env >> "$LOG_FILE"
 echo "Script execution initiated. The copy operation will run in the background." >> "$LOG_FILE"
 
 # Path to the background script
-BACKGROUND_SCRIPT="/home/nas/Documents/background_copy_sd.sh"
+BACKGROUND_SCRIPT="[PATH TO BACKGROUND SCRIPT]/background_copy_sd.sh"
 
 # Run the background process asynchronously
 nohup "$BACKGROUND_SCRIPT" "$MOUNT_POINT" "$LOG_FILE" >> "$LOG_FILE" 2>&1 &
